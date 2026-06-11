321db18944fa2d4dd933d837fd197175f1150ef5
Selva S <selva.it21@gmail.com>
2026-06-11 17:17:25 +0530

    Error fixed

--- server/models/Attendance.js
+++ server/models/Attendance.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const attendanceSchema = new mongoose.Schema({
     employeeId: { type: mongoose.Schema.Types.ObjectId, ref: "Employee", required: true },
     date: { type: Date, required: true },
-    checkIn: { type: Date, default: true },
+    checkIn: { type: Date, default: null },
     checkOut: { type: Date, default: null },
     status: { type: String, enum: ["PRESENT", "ABSENT", "LATE"], default: "PRESENT" },
     workingHours: { type: Number, default: null },
